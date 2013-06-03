328db7da8acd52a3a83ce123793be6f2fd94a150
vishnu.kanduri <vishnu.kanduri@maxsoftware.com.au>
2013-06-03 13:27:54 +1000

    added files to the git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learn::Application.config.secret_token = '83d6bb188b826c3f8122c231338e21e26c12b2e50ebdf87ce8f48280c26f0616e2d6e4e0f07e1b58abf76938627d224bbef0dc4986e5812b15a3fa230af372ee'
